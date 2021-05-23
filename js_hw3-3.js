d3de3ae672bd335b5b26e4e417486e2f022fd572
abaibik <abaibik@gmail.com>
2021-05-23 20:25:22 +0200

    made task 3 of lesson3

---
+++ js_hw3-3.js
@@ -0,0 +1,21 @@
+const products = [
+  {
+    id: 3,
+    price: 200,
+  },
+  {
+    id: 4,
+    price: 900,
+  },
+  {
+    id: 1,
+    price: 1000,
+  },
+];
+
+const sale = products.map(function (product) {
+  product.price *= 0.85;
+  return product;
+});
+
+console.log(sale);
