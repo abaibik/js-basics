59f346132d90235a1f4dbbf0e29ca530277678a5
abaibik <abaibik@gmail.com>
2021-05-24 16:24:05 +0200

    task 4

---
+++ js_hw3-4.js
@@ -0,0 +1,30 @@
+const products = [
+  {
+    id: 3,
+    price: 127,
+    photos: ["1.jpg", "2.jpg"],
+  },
+  {
+    id: 5,
+    price: 499,
+    photos: [],
+  },
+  {
+    id: 10,
+    price: 26,
+    photos: ["3.jpg"],
+  },
+  {
+    id: 8,
+    price: 78,
+  },
+];
+
+const productsWithPhotoProp = products.filter((product) =>
+  product.hasOwnProperty("photos")
+);
+const productsWithPhotos = productsWithPhotoProp.filter(
+  (product) => product.photos.length > 0
+);
+const result = productsWithPhotos.sort((a, b) => a.price - b.price);
+console.log(result);
