e4252a47a08d36753ec4bb760c12b651225bba8a
abaibik <abaibik@gmail.com>
2021-05-27 17:22:44 +0200

    game over

--- billioner/game.js
+++ billioner/game.js
@@ -1,4 +1,4 @@
-const game = {
+class Game {
   run() {
     questionArray.forEach(function (value) {
       let message = `${value.question}\n`;
@@ -6,7 +6,32 @@ const game = {
         const protoAnswer = `${index + 1}. ${answer}\n`;
         message += protoAnswer;
       });
-      prompt(message);
-    });
-  },
-};
+
+      const availableAnswers = [1, 2, 3, 4];
+
+      while (true) {
+        const userAnswer = parseInt(prompt(message));
+        if (isNaN(userAnswer)) {
+          return;
+        }
+        if (!availableAnswers.includes(userAnswer)) {
+          alert("Необходимо ввести одно из чисел: 1, 2, 3, или 4.");
+          continue;
+        }
+
+        this.check(userAnswer - 1, value.rightAnswer);
+        break;
+      }
+    }, this);
+
+    alert(
+      `Поздравляем, игра окончена! Вы ответили правильно на ${player.score} из ${questionArray.length} вопросов.`
+    );
+  }
+
+  check(userAnswer, rightAnswer) {
+    if (userAnswer === rightAnswer) {
+      player.score++;
+    }
+  }
+}
